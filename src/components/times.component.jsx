3dcb7474b532aab4cbf6999d81cbacd4f8a74c00
Noyan Alimov <noyan.alimov@gmail.com>
2020-10-18 20:33:34 +0100

    trying to figure out how to handle multiple checkboxes

--- src/components/times.component.jsx
+++ src/components/times.component.jsx
@@ -2,33 +2,44 @@ import React from 'react';
 import { timess } from "../data";
 
 export const Times = ({ match }) => {
-    const [times, setTimes] = React.useState({});
-
     const chosenDate = match.params.dateId;
     const availableTimes = timess.filter(time => time.dateId === parseInt(chosenDate));
 
-    const handleChange = e => {
+    const [times, setTimes] = React.useState({availableTimes});
 
+    const handleChange = e => {
+        // setTimes([...times, [e.target.name]: e.target.checked]);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(times);
     };
 
     return (
         <div className='container'>
             <div className='row'>
-                <form className='col s12'>
+                <form className='col s12' onSubmit={handleSubmit}>
                     <div className='row'>
-                        {
-                            availableTimes.map(time => (
-                                <div key={time.id} className='col s12'>
-                                    <p>
-                                        <label>
-                                            <input type="checkbox" name={time.id} value={time.time} />
-                                            <span>{time.time}</span>
-                                        </label>
-                                    </p>
-                                </div>
-                            ))
-                        }
+                    {
+                        availableTimes.map(time => (
+                            <div key={time.id} className='col s12'>
+                                <p>
+                                    <label>
+                                        <input 
+                                            type="checkbox" 
+                                            name='time' 
+                                            checked={time.booked}
+                                            onChange={handleChange}
+                                        />
+                                        <span>{time.time}</span>
+                                    </label>
+                                </p>
+                            </div>
+                        ))
+                    }
                     </div>
+                    <button type='submit'>Book</button>
                 </form>
             </div>
         </div>
